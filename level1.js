cb15090c4362abe06e21a6364ee743cc5f315344
Andrea  Piazza <apiazza023@gmail.com>
2018-11-06 19:04:05 -0800

    search on an array with occurance in array

--- level1.js
+++ level1.js
@@ -19,6 +19,49 @@ leaf = (str1, str2) => {
     }
     return newStr
   }
-  
-  
+
   leaf('abc','def')
+
+  //return array with only even characters 
+
+evenChars = (string) => {
+    words = string.split(" ")
+    newString = []
+    for (let i = 0; i < words.length; i ++){
+     if( words[i].length % 2 == 0 ){
+       newString.push(words[i])
+     }
+    }
+    return newString
+  }
+  evenChars("This is an example of my string list.")
+
+  // Implement search on an array (given element of array - give index of element in arr) 
+
+searchArr = (val, arr) => {
+    for (var i =0; i < arr.length; i++){
+      if(val == arr[i]){
+        return i
+      }
+    }  
+    return -1
+  } 
+  searchArr(4, [2, 3, 4, 5, 19, 4, 23, 0])
+
+  // Implement search on an array (given element of array - give index of element in arr) 
+
+searchArr = (val, occur, arr) => {
+    count = 0; 
+    for (var i =0; i < arr.length; i++){
+        if(val == arr[i]){
+        count += 1 
+        console.log('count'+ count)
+        if (occur == count){
+          return i 
+          console.log('Index' + i)
+        }
+        }
+      } 
+    return -1
+  } 
+  searchArr(4, 2, [2, 3, 4, 5, 19, 4, 23, 0, 4])
